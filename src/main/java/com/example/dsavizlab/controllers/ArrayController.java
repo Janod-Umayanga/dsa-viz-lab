9123423751e442059b5bd96df59c6b78ab737a56
Janod-Umayanga <87465214+Janod-Umayanga@users.noreply.github.com>
2024-06-16 01:28:11 +0530

    display the indices of the array on array view.

--- src/main/java/com/example/dsavizlab/controllers/ArrayController.java
+++ src/main/java/com/example/dsavizlab/controllers/ArrayController.java
@@ -1,12 +1,12 @@
 package com.example.dsavizlab.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-import java.io.IOException;
 import java.util.Random;
 
 public class ArrayController extends MainController{
@@ -29,7 +29,7 @@ public class ArrayController extends MainController{
     }
 
     @FXML
-    protected void OnGenerateArrayClicked(ActionEvent event) throws IOException {
+    protected void OnGenerateArrayClicked() {
         String UserInput = UserInputArrayLength.getText();
         int ArrayLength = Integer.parseInt(UserInput);
 
@@ -52,10 +52,24 @@ public class ArrayController extends MainController{
     private void VisualizeArray (int[] Array) {
         ArrayView.getChildren().clear();
 
+        int Index = 0;
+
         for (int Element : Array) {
-            Label ElementContainer = new Label(String.valueOf(Element));
-            ElementContainer.setStyle("-fx-border-color: black; -fx-padding: 5px;");
+            VBox ElementContainer = new VBox();
+
+            Label ElementValue = new Label(String.valueOf(Element));
+            Label ElementIndex = new Label(String.valueOf(Index));
+
+            ElementValue.setStyle("-fx-border-color: black; -fx-padding: 5px;");
+
+            ElementContainer.getChildren().add(ElementValue);
+            ElementContainer.getChildren().add(ElementIndex);
+            ElementContainer.setAlignment(Pos.valueOf("CENTER"));
+            ElementContainer.setSpacing(5.0);
+
             ArrayView.getChildren().add(ElementContainer);
+
+            Index++;
         }
     }
 }
