bc7f04966eed767514b54c3b833b9364cd69cba8
Janod-Umayanga <87465214+Janod-Umayanga@users.noreply.github.com>
2024-06-19 02:16:43 +0530

    selection sort (without visualization) of an array.

--- src/main/java/com/example/dsavizlab/controllers/ArrayController.java
+++ src/main/java/com/example/dsavizlab/controllers/ArrayController.java
@@ -11,11 +11,19 @@ import java.util.Random;
 
 public class ArrayController extends MainController{
 
+    private int[] Array;
+
     @FXML
     private TextField UserInputArrayLength;
 
     @FXML
     private HBox ArrayView;
+
+    @FXML
+    private HBox ArraySortingSection;
+
+    @FXML
+    private HBox SortedArrayView;
 
     @FXML
     public void initialize() {
@@ -33,9 +41,19 @@ public class ArrayController extends MainController{
         String UserInput = UserInputArrayLength.getText();
         int ArrayLength = Integer.parseInt(UserInput);
 
-        int[] Array = GenerateRandomIntegerArray(ArrayLength);
+        Array = GenerateRandomIntegerArray(ArrayLength);
 
-        VisualizeArray(Array);
+        VisualizeArray(Array, ArrayView);
+
+        // Display sorting algorithm section if there is an array
+        ArraySortingSection.setVisible(Array.length > 0);
+    }
+
+    @FXML
+    protected void OnSelectionSortClicked() {
+        int[] SortedArray = SelectionSortArray(Array);
+
+        VisualizeArray(SortedArray, SortedArrayView);
     }
 
     private int[] GenerateRandomIntegerArray(int ArrayLength) {
@@ -49,7 +67,7 @@ public class ArrayController extends MainController{
         return Array;
     }
 
-    private void VisualizeArray (int[] Array) {
+    private void VisualizeArray (int[] Array, HBox ArrayView) {
         ArrayView.getChildren().clear();
 
         int Index = 0;
@@ -72,4 +90,31 @@ public class ArrayController extends MainController{
             Index++;
         }
     }
+
+    // Array Sorting Algorithms
+    private int[] SelectionSortArray (int[] Array) {
+        int[] SortedArray;
+        SortedArray = Array;
+
+        int ArrayLength = Array.length;
+
+        for (int i = 0; i < ArrayLength - 1; i++) { // Incrementing the index of unsorted part of the array
+            // Finding the index of the minimum value of the unsorted part of the array
+            int IndexOfMinValue = i;
+
+            for (int j = i +1; j < ArrayLength; j++) {
+                if (SortedArray[j] < SortedArray[IndexOfMinValue]) {
+                    IndexOfMinValue = j;
+                }
+            }
+
+            // Swap the minimum value of the unsorted part with the first position of the unsorted part
+            int Temp = SortedArray[i];
+            SortedArray[i] = SortedArray[IndexOfMinValue];
+            SortedArray[IndexOfMinValue] = Temp;
+        }
+
+        return SortedArray;
+    }
+
 }
